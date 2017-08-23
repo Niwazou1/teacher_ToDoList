192028cc9c3849749ad1ea23f87f5e74f86cb22e
Niwazou1 <yuunosuke_16n1101227@nnn.ed.jp>
2017-08-23 13:37:34 +0900

    complete

--- frontend/views/index.js
+++ frontend/views/index.js
@@ -2,5 +2,33 @@ const domready = require('domready');
 const Vue = require('vue/dist/vue.min');
 
 domready(function(){
-    console.log('hello');
+    let todo = document.getElementById('todo');
+    if(!todo){
+        return;
+    }
+
+    let vm = new Vue({
+        el:"#todo",
+        data: {
+            newTodo: { body: '' },
+            todos: []
+        },
+        methods: {
+            createToDo: function(event){
+                event.preventDefault();
+                let value = this.newTodo.body && this.newTodo.body.trim();
+                if(!value){
+                    return;
+                }
+                this.todos.push({
+                    body: this.newTodo.body,
+                    isCompleted: false
+                });
+                this.newTodo.body = '';
+            },
+            deleteTodo: function(todo){
+                this.todos.splice(this.todos.indexOf(todo), 1);
+            }
+        }
+    });
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
-    app.get('/', function(req, res){
+    app.get('/todo', function(req, res){
         res.render('todo');
       });
   };
