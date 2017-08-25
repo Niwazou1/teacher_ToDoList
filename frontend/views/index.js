a66181f38babb51cfc3653f68c7ddefd3b3aa693
Niwazou1 <yuunosuke_16n1101227@nnn.ed.jp>
2017-08-25 14:56:20 +0900

    oi

--- frontend/views/index.js
+++ frontend/views/index.js
@@ -11,14 +11,14 @@ domready(function(){
         el:"#todo",
         data: {
             newTodo: { body: '' },
-            todos: []
+            todos: JSON.parse(localStorage.getItem('phh-todos')) || []
         },
         methods: {
-            createToDo: function(event){
+            createTodo: function(event){
                 event.preventDefault();
                 let value = this.newTodo.body && this.newTodo.body.trim();
                 if(!value){
-                    return;
+                    return; 
                 }
                 this.todos.push({
                     body: this.newTodo.body,
@@ -28,7 +28,33 @@ domready(function(){
             },
             deleteTodo: function(todo){
                 this.todos.splice(this.todos.indexOf(todo), 1);
+            },
+            purgeTodo: function(){
+                this.todos = this.todos.filter(function(todo){
+                    return !todo.isCompleted;
+                });
+            }   
+        },
+        computed: {
+            activeTodos: function(){
+                return this.todos.filter(function(todo){
+                    return !todo.isCompleted;
+                });
+            },
+            completedTodos: function(){
+                return this.todos.filter(function(todo){
+                    return todo.isCompleted;
+                })
             }
+        },
+        watch: {
+            todos: {
+                handler: function(val){
+                let todos_json = JSON.stringify(this.todos);
+                localStorage.setItem('phh-todos', todos_json);
+                }
+            },
+            deep: true
         }
     });
 });
